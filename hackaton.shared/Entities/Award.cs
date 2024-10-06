a13713be1415360d2efd409aef6d7dea51527d29
Maria Camila <camila.arroyoa@gmail.com>
2024-10-06 15:09:06 -0500

    "fix foreing keys"

---
+++ hackaton.shared/Entities/Award.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Hackaton.shared.Entities
+{
+    public class Award
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Premio a otorgar")]
+        [Required]
+        [MaxLength(100, ErrorMessage = "EL nombre debe ser menor a 100 caracteres")]
+        public string Awards { get; set; }
+
+        [Display(Name = "Puesto")]
+        [Required]
+        public string Position { get; set; }
+
+        [JsonIgnore]
+        public Hackaton Hackatons { get; set; }
+        public int HackatonId { get; set; }
+
+
+    }
+}
--- hackaton.shared/Entities/Participantes.cs
+++ hackaton.shared/Entities/Participantes.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Hackaton.shared.Entities
 {
-    internal class Participantes
+    public class Participantes
     {
         public int Id { get; set; }
 
@@ -19,16 +20,15 @@ namespace Hackaton.shared.Entities
         [Display(Name = "Experiencia")]
         [Required]
         [MaxLength(50, ErrorMessage = "EL nombre debe ser menor a 50 caracteres")]
-        public string Experiencia { get; set; }
+        public string expertise { get; set; }
 
-        [Display(Name = "Rol")]
-        [Required]
-        [MaxLength(50, ErrorMessage = "EL Rol debe ser menor a 50 caracteres")]
-        public string rol { get; set; }
+        [JsonIgnore]
+        public Rol Roles { get; set; }
+        public int RolId { get; set; }
 
-        [Display(Name = "Id Equipoo")]
-        [Required]
-        [MaxLength(50, ErrorMessage = "EL ID del equipo debe ser menor a 50 caracteres")]
-        public string Id_Equipo { get; set; }
+        [JsonIgnore]
+        public Team Teams { get; set; }
+        public int TeamId { get; set; }
+
     }
 }
