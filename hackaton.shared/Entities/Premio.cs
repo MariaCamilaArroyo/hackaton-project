c8ac3950ef49d8663ac54e437fd618850ac11be7
MauricioSAlazar96 <javiersalazar1130499@correo.itm.edu.co>
2024-10-06 14:23:18 -0500

    Incluyendo la clase premio

---
+++ hackaton.shared/Entities/Premio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackaton.shared.Entities
+{
+    internal class Premio
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Premio Obtenido")]
+        [Required]
+        [MaxLength(100, ErrorMessage = "EL nombre debe ser menor a 100 caracteres")]
+        public string PremioObtenido { get; set; }
+
+
+
+
+
+
+
+
+
+
+    }
+}
