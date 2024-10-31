6cafb1d81212f475c2c7d647ce9654c219cb9012
Maria Camila <camila.arroyoa@gmail.com>
2024-10-30 23:01:16 -0500

    Add Repository

---
+++ Hackaton.WEB/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Hackaton.WEB.Repository;
+
+public interface IRepository
+{
+    Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
+    Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model);
+
+    Task<HttpResponseWrapper<TResponse>> PostAsync<T, TResponse>(string url, T model);
+
+    Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model);
+
+    Task<HttpResponseWrapper<TResponse>> PutAsync<T, TResponse>(string url, T model);
+
+    Task<HttpResponseWrapper<object>> DeleteAsync(string url);
+}
