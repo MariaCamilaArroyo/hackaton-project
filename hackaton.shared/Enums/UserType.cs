d5866ef6e862beb9b011b9243a4fcf009c3f7d4d
MauricioSAlazar96 <javiersalazar1130499@correo.itm.edu.co>
2024-11-02 12:08:44 -0500

    Actualizacion

---
+++ hackaton.shared/Enums/UserType.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.PowerBI.Api;
+using Microsoft.PowerBI.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackaton.shared.Enums
+{
+    public enum UserType
+    {
+        User,Admin
+    }
+}
